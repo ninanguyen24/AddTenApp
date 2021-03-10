88b13d7d8619cdd893ca883e5d8d33a7bba7862e
Nina Nguyen <nguyenn4@seattleu.edu>
2021-03-10 10:08:13 -0800

    Setting manually

--- src/main/java/com/addten/bootcamp/BootcampApplication.java
+++ src/main/java/com/addten/bootcamp/BootcampApplication.java
@@ -3,11 +3,20 @@ package com.addten.bootcamp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Properties;
+
 @SpringBootApplication
 public class BootcampApplication {
 
 	public static void main(String[] args) {
+		Properties properties;
+		properties = System.getProperties();
+		properties.setProperty("java.runtime.version", "15");
+		System.setProperties(properties);
+		//properties.list(System.out);
+
 		SpringApplication.run(BootcampApplication.class, args);
+
 	}
 
 }
